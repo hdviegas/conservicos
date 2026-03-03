a90cecb6a44db7bb83ca9f0bf67cddbc7e7490a9
Hilthermann Viegas <hviegas@meetsoci.com>
2026-03-02 22:19:48 -0300

    fix main route

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ReportController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin');
 });
 
 Route::prefix('admin/reports')->name('reports.')->middleware(['web', 'auth'])->group(function () {
